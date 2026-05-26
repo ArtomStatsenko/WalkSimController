1601719673a00e146acabd4e38f22c668fbdf02d
ArtomStatsenko <namebyuser@gmail.com>
2026-05-26 11:58:51 +0700

    Sync Unity sample fixes

--- samples/Unity/PlayerUnityAdapter.cs
+++ samples/Unity/PlayerUnityAdapter.cs
@@ -1,8 +1,5 @@
-using System.Numerics;
 using UnityEngine;
 using WalkSimController;
-using CoreVector2 = System.Numerics.Vector2;
-using CoreVector3 = System.Numerics.Vector3;
 
 [RequireComponent(typeof(CharacterController))]
 public sealed class PlayerUnityAdapter : MonoBehaviour
@@ -13,43 +10,42 @@ public sealed class PlayerUnityAdapter : MonoBehaviour
     [SerializeField]
     private Camera playerCamera = null!;
 
-    private readonly WalkSimController.WalkSimController controller = new(CoreVector3.Zero);
-    private CharacterController character = null!;
+    private readonly WalkSimController.WalkSimController _controller =
+        new(System.Numerics.Vector3.Zero);
+    private CharacterController _character = null!;
 
     private void Awake()
     {
-        character = GetComponent<CharacterController>();
+        _character = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void Update()
     {
-        var state = controller.Update(Time.deltaTime, GatherInput());
+        var state = _controller.Update(Time.deltaTime, GatherInput());
 
-        var velocity = new UnityEngine.Vector3(
-            state.BodyVelocity.X,
-            Physics.gravity.y,
-            state.BodyVelocity.Z
-        );
+        var velocity = new Vector3(state.BodyVelocity.X, Physics.gravity.y, state.BodyVelocity.Z);
 
-        character.Move(velocity * Time.deltaTime);
-        controller.SyncBodyPosition(ToCore(transform.position));
+        _character.Move(velocity * Time.deltaTime);
+        _controller.SyncBodyPosition(ToCore(transform.position));
 
         ApplyCamera(state);
     }
 
-    private static FrameInput GatherInput() =>
-        new()
+    private static FrameInput GatherInput()
+    {
+        return new FrameInput
         {
-            MoveAxis = new CoreVector2(
+            MoveAxis = new System.Numerics.Vector2(
                 Input.GetAxisRaw("Horizontal"),
                 Input.GetAxisRaw("Vertical")
             ),
-            MouseDelta = new CoreVector2(
+            MouseDelta = new System.Numerics.Vector2(
                 Input.GetAxisRaw("Mouse X"),
                 Input.GetAxisRaw("Mouse Y")
             ),
         };
+    }
 
     private void ApplyCamera(WalkSimState state)
     {
@@ -62,14 +58,14 @@ public sealed class PlayerUnityAdapter : MonoBehaviour
             state.CameraRotation.Roll
         );
 
-        playerCamera.transform.localPosition = new UnityEngine.Vector3(
+        playerCamera.transform.localPosition = new Vector3(
             state.CameraLocalOffset.X,
             state.CameraLocalOffset.Y,
             state.CameraLocalOffset.Z
         );
     }
 
-    private static CoreVector3 ToCore(UnityEngine.Vector3 v) => new(v.x, v.y, v.z);
+    private static System.Numerics.Vector3 ToCore(Vector3 v) => new(v.x, v.y, v.z);
 
     private static float DeltaAngleDeg(float from, float to) => Mathf.DeltaAngle(from, to);
 }
